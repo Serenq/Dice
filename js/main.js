55708551d321d0e3cd6c08b416ac16bac642f4ed
Serenq <tiotiokan@gmail.com>
2023-04-19 16:56:44 +0300

    только стили кубика, стили цифр кубика

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/* 
+ * Игральные кости by Serenq / 19 апреля 2023
+*/
+
+;(function(){
+    const project = {
+        title: 'Игральные кости',
+        author: 'Serenq',
+        version: '1.0',
+        info: function(){
+            console.log(`%c${this.title} ${this.version} / by ${this.author}`, "color: #ACE600; font-style: italic; background-color: #444; padding: 0 20px");
+        }
+    }
+
+    let dice = {
+        
+    }
+
+    project.info();
+}());
